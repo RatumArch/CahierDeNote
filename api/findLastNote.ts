0623fe6efbea7b32096c0217f53d82f336353292
Totaroo <muigni-hazi.mze@cpe.fr>
2022-06-23 21:34:14 +0200

    katex installation

--- api/findLastNote.ts
+++ api/findLastNote.ts
@@ -3,17 +3,28 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { _get, _post } from '../utils/index.js'
 import { PrismaClient } from '@prisma/client'
 
+import sanitizeHtml from 'sanitize-html';
+import katex from 'katex';
 
 
 async function findLast(req: any, res: VercelResponse) {
     const prisma = new PrismaClient()
-    const results: any = await prisma.notes.findMany({ 
+    let results: any = await prisma.notes.findMany({ 
         orderBy: [ { id: 'desc' }],
         take: 1
-    }).catch(err => { console.error("requête ratée");
     })
+    .catch(err => { console.error("requête ratée");
+    })
+    const expr = katex.renderToString('(x^2 + y/2 = z/2)');    
 
+    results[0].html = expr +'<br/>'+ sanitizeHtml( expr, {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['image-input']),
+        allowedAttributes: {"image-input": ['src', 'nodeId']}
+    })
+    
+  
     const lastDoc = results[0]
+    
     res.status(200).send(lastDoc)
     prisma.$disconnect()
 }
