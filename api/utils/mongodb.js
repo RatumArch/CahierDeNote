98bd1a85eb5c132560a7f1531bae14ae0176a9cb
Totaroo <muigni-hazi.mze@cpe.fr>
2022-07-01 21:27:44 +0200

    correction

---
+++ api/utils/mongodb.js
@@ -0,0 +1,38 @@
+import { MongoClient } from 'mongodb'
+
+const uri = process.env.MONGODB_URI
+const collectionName = process.env.MONGODB_DB_COLLECTION
+const options = {}
+
+let client
+let clientPromise
+
+if (!process.env.MONGODB_URI) {
+    throw new Error('Please add your Mongo URI to .env.local')
+}
+
+if (process.env.DEV) {
+    // In development mode, use a global variable so that the value
+    // is preserved across module reloads caused by HMR (Hot Module Replacement).
+    if (!global._mongoClientPromise) {
+        client = new MongoClient(uri, options)
+        global._mongoClientPromise = client.connect()
+    }
+    clientPromise = global._mongoClientPromise
+} else {
+    // In production mode, it's best to not use a global variable.
+    client = new MongoClient(uri, options)
+    clientPromise = client.connect()
+}
+
+const db = process.env.MONGODB_DB
+
+export const createCollection = async (clientPr, name) => {
+    const client = await clientPr.then((client) => client)
+    const datab = client.db(db)
+    return datab.collection(name)
+}
+
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
+export default clientPromise
---
+++ api/utils/serialize.ts
@@ -0,0 +1,22 @@
+import {Collection, ObjectId, Sort} from "mongodb";
+// @ts-ignore
+import clientPromise from "./mongodb.js";
+/**
+ * Fonction à appliquer sur les objets ayant un cham "_id: new ObjectId('___')"
+ * Retourne un objet JavaScript valide
+ */
+ export function mapSerializeDocs(docsArray: any) {
+    return docsArray.map((doc: any) => {
+        doc._id = doc._id.toString()
+        return doc
+    })
+}
+
+export function serializeDoc(document: any) {
+    let newDoc = document;
+    let id = document?._id
+    // On vérifie si '_id' est de la forme 'new ObjectId'
+    if(ObjectId.isValid(id)) {newDoc._id = document?._id.toString()}
+
+    return newDoc;
+}
