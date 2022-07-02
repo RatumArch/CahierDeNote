c8eb5b3b88ec159a2af8ce5144d42d0eacb5d048
Totaroo <muigni-hazi.mze@cpe.fr>
2022-07-02 21:40:28 +0200

    refactor

--- api/updateNote.ts
+++ api/updateNote.ts
@@ -1,4 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { Collection } from 'mongodb';
 import { _put } from '../utils'
 //@ts-ignore
 import {clientPromise} from "../utils";
@@ -33,14 +34,7 @@ async function update(req:VercelRequest, res: VercelResponse) {
     res.status(200).send(updated)
 }
 
-async function updateTransaction(req:VercelRequest, res: VercelResponse) {
-    const params = req.query
-    const title = params?.title
-    const folderCode= params?.folderCode
-    const raw = params?.raw
-    const html = params?.html
-    const newTitle = params?.newTitle
-    
+async function updateTransaction(req:VercelRequest, res: VercelResponse) {    
     //@ts-ignore
     const client = await clientPromise.then((client: any) => client)
     const datab = client.db(db)
@@ -55,8 +49,33 @@ async function updateTransaction(req:VercelRequest, res: VercelResponse) {
 
     const session = client.startSession()
 
-    await session.withTransaction(async (mySession: any) => {
-        const query= { title, folderCode }
+    await session.withTransaction(async (mySession: any) => 
+        await transaction(mySession, collection, req, res), transactionOptions
+    )
+      .then(async (updated: any) => { 
+            res.status(200).send({status: 'success', message: 'Transaction efféctuée'})
+            return await session.endSession()
+        })
+      .catch((err: any) => {
+            res.status(408).send({status: 'error', message: 'Transaction ratée', error: err})
+            console.error(err)
+            session.endSession()
+        })
+
+}
+
+const updateNote = (req:VercelRequest, res:VercelResponse) => _put(updateTransaction, req, res)
+export default updateNote
+
+async function transaction(mySession: any, collection: Collection, req:VercelRequest, res:VercelResponse) {
+    const params = req.body
+    const title = params?.title
+    const folderCode= params?.folderCode
+    const raw = params?.raw
+    const html = params?.html
+    const newTitle = params?.newTitle
+
+    const query= { title, folderCode }
         const fieldsToUpdate: any= {
             $set: {
                 raw,
@@ -70,27 +89,12 @@ async function updateTransaction(req:VercelRequest, res: VercelResponse) {
             const findTitle = await collection.findOne({ title: newTitle, folderCode}, {session: mySession})
             
             if(findTitle) {
-                res.status(400).send({error: "Il y a déjà un fichier avec ce nom dans ce dossier"})
-                await mySession.abortTransaction().then(() => { console.log("transaction aborted fini car même titre");
-                 })
+                await mySession.abortTransaction()
+                res.status(400).send({message: "Il y a déjà un fichier avec ce nom dans ce dossier", status: 'error'})
             }
             fieldsToUpdate.$set.title= newTitle
         }
 
         const updated = await collection.updateOne(query, fieldsToUpdate, { session: mySession}).catch(() => "Update failed")
         return updated
-        
-    }, transactionOptions)
-      .then(async (updated: any) => { 
-            res.status(200).send(updated)
-            return await session.endSession()
-        })
-      .catch((err: any) => {
-            res.status(408).send(err)
-            console.error(err)
-        })
-
 }
-
-const updateNote = (req:VercelRequest, res:VercelResponse) => _put(updateTransaction, req, res)
-export default updateNote
