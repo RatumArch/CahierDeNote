aa9b1f9951565f6a45c8ac8e6544ab9ac50d279c
Totaroo <muigni-hazi.mze@cpe.fr>
2022-07-02 14:20:20 +0200

    update2

--- api/updateNote.ts
+++ api/updateNote.ts
@@ -1,6 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { PrismaClient } from '@prisma/client'
 import { _put } from '../utils'
+//@ts-ignore
+import {clientPromise} from "../utils";
+
+const db = process.env.MONGODB_DB
+const collec = process.env.MONGODB_DB_COLLECTION
 
 async function update(req:VercelRequest, res: VercelResponse) {
     const prisma = new PrismaClient()
@@ -16,7 +21,7 @@ async function update(req:VercelRequest, res: VercelResponse) {
             folderCode
         }
     })
-    const updated = await prisma.notes.update({
+    const updated = await prisma.notes.updateMany({
         where: {
             title: title,
             folderId: folder?.id
@@ -31,5 +36,31 @@ async function update(req:VercelRequest, res: VercelResponse) {
     res.status(200).send(updated)
 }
 
-const updateNote = (req:VercelRequest, res:VercelResponse) => _put(update, req, res)
+async function update2(req:VercelRequest, res: VercelResponse) {
+    const params = req.body
+    const title = params?.title
+    const folderCode= params?.folderCode
+    const raw = params?.raw
+    const html = params?.html
+    const newTitle= params?.newTitle
+
+    //@ts-ignore
+    const client = await clientPromise.then((client: any) => client)
+    const datab = client.db(db)
+    const collection = await datab.collection(collec)
+
+    const folderId = await datab.collection(collec).findOne({ folderCode }).then((folder: any) => folder?.id).catch(() => null)
+    const query= { title, folderId }
+    const updated = await collection.updateOne(query, {
+        $set: {
+            raw,
+            html,
+            title: newTitle
+        }
+    }).catch(() => "Update failed")
+
+    res.status(200).send(updated)
+}
+
+const updateNote = (req:VercelRequest, res:VercelResponse) => _put(update2, req, res)
 export default updateNote
