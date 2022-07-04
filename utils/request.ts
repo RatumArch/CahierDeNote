727d67d0f067ed43fa0da7d49eea256092e6cd5f
Totaroo <muigni-hazi.mze@cpe.fr>
2022-07-04 23:02:24 +0200

    request

---
+++ utils/request.ts
@@ -0,0 +1,21 @@
+import axios from "axios"
+
+/**
+ * Envoie une requête PUT pour mettre enregistrer les modifications du documents.
+ * @param title Titre du document à enregistrer
+ * @param folderCode Dossier contenant le document
+ * @param htmlContent 
+ * @param rawText 
+ * @param newTitle Optionnel. Nouveau titre à donner au document
+ * @param extra Données additionnelles
+ */
+export async function saveDocument(title: string, folderCode: string, htmlContent: string, rawText: string, newTitle?: string,  extra?: any) {
+    return await axios.put('/api/updateNote', {
+          title,
+          folderCode,
+          newTitle,
+          html: htmlContent,
+          raw: rawText,
+          extra
+        })
+}   
