4539f93c9bbccd58fe8825ab041619561b7265bc
Totaroo <muigni-hazi.mze@cpe.fr>
2022-07-06 16:21:40 +0200

    api findfolder

--- api/findFolder.ts
+++ api/findFolder.ts
@@ -1,30 +1,40 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { PrismaClient } from '@prisma/client'
-import { _get, sanitizeText } from '../utils/index.js'}
+import { notes, PrismaClient } from '@prisma/client'
+// @ts-ignore
+import { _get, clientPromise, sanitizeText } from '../utils/index.js'
+import { Collection } from 'mongodb';
+
+const prisma = new PrismaClient()
+const db = process.env.MONGODB_DB
+const notesCollec = process.env.MONGODB_DB_COLLECTION
+const foldersCollection= process.env.MONGODB_DB_FOLDER_COLLECTION
 
 async function findFolder(req: VercelRequest, res: VercelResponse) {
     const prisma = new PrismaClient()
 
+    //@ts-ignore
+    const client = await clientPromise.then((client: any) => client)
+    const datab = client.db(db)
+    const collection: Collection = await datab.collection(notesCollec)
+
     const folderCode = <string>req.query?.folderCode
     
-    const folder = await prisma.folders.findMany({
-        where: {
-            folderCode
-        },
-        include: {
-            notesContent: true
-        }
+    const notes = await collection.find({
+        folderCode
     })
+    .sort({ modifiedDate: -1 })
+    .toArray()
+    .then((notes) => notes.map((note: any) => { note.html = sanitizeText(note.html); return note } ) )
     .catch((err) => { 
         res.status(404).send("Ce dossier est introuvable")
         return null
      })
-console.log(folder?.[0]?.notesContent);console.log("/folder");
+console.log(notes);console.log("/folder");
 // @ts-ignore
 //folder[0].notesContent = folder?.[0]?.notesContent.map((note) => { note.html = sanitizeText(note?.html); return note}) ?? 
 
     prisma.$disconnect()
-    res.status(200).send(folder?.[0])
+    res.status(200).send(notes)
 }
 
 const findFolder2 = (req: VercelRequest, res: VercelResponse) => _get(findFolder, req, res)
