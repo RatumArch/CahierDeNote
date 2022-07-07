0b431dbebf2cc449b57c2a6e57d5727617b0a6c0
Totaroo <muigni-hazi.mze@cpe.fr>
2022-07-07 04:40:22 +0200

    css

--- src/Loader.vue
+++ src/Loader.vue
@@ -17,13 +17,18 @@ h1 {
     -webkit-text-stroke-color: black;
 }
 .loader {
-    padding: 20vw;
+    padding: 5vw;
     width: 90vw;
     height: 90vh;
     opacity: 0.7;
     background-color: blanchedalmond;
     position: fixed;
     top: 10vh;
+    left: 0px;
+
+    animation-name: loading-anim;
+    animation-iteration-count: infinite;
+    animation-duration: 0.8s;
 }
 .pancarte {
     opacity: 1;
@@ -32,5 +37,38 @@ h1 {
 }
 progress {
     width: 100%;
+}
+@keyframes loading-anim {
+  from {
+    background: linear-gradient(to right, darkgreen 75%, lightblue);
+    opacity: 0.2;
+  }
+  10% {
+    background: linear-gradient(to right, darkgreen 50%, lightblue);
+    opacity: 0.4;
+  }
+  25% {
+    background: linear-gradient(to right, darkgreen 50%, lightblue);
+    opacity: 0.8;
+  }
+  50% {
+    background: linear-gradient(to right, darkgreen 25%, lightblue);
+    opacity: 1;
+  }
+  75% {
+    background: linear-gradient(to right, darkgreen 10%, lightblue);
+    opacity: 0.8;
+  }
+  from {
+    background: darkgreen;
+    opacity: 0.4;
+  }
+}
+
+.editable-title {
+  border: none;
+  font-size: inherit;
+  padding-left: 5px;
+  width: 100%;
 }
 </style>
