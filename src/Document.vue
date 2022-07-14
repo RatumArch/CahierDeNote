8673f8066f440581d283062ea0a246dd938c9ed1
Totaroo <muigni-hazi.mze@cpe.fr>
2022-07-14 23:17:37 +0200

    dataloaded

--- src/Document.vue
+++ src/Document.vue
@@ -2,7 +2,7 @@
   <h1><input type="text" placeholder="Titre" class="editable-title" v-model="editableTitle" /></h1>
   <div>
       <div>
-        <button type="button" @click="toggleAutoSave" class="auto-save" :class="{disabled: !autoSaveEnabled, onSave: isSaveLoading&&autoSaveEnabled}" >
+        <button type="button" @click="toggleAutoSave" class="auto-save" :class="{disabled: !autoSaveEnabled, onSave: isSaveLoading}" >
           Auto save
         </button>
         <Loader v-if="isLoading" />
@@ -58,7 +58,7 @@ function setMessageServer(msg: string) {
   messageFromServer.value=msg
   const timeout = setTimeout(() => {messageFromServer.value=''; }, 5000)
 }
-
+const logg = () => { console.log("Documentv - event received")}
 const newTitle = computed(() => editableTitle.value!==title.value ? <string> editableTitle.value : null )
 const sendToMongo = async (html: string, raw: string, extra?: object) => { 
   isSaveLoading.value=true
@@ -96,7 +96,15 @@ async function toggleAutoSave() {
 }
 
 onMounted(async () => {
-    isDataLoaded.value = true
+    if(!isDataLoaded.value)
+    {
+      console.log(route.params);console.log(route.params?.document ?? "no param document")
+      const data = await getContent(<string>folderCode.value, <string>route.params?.document)
+      content.value = data?.html ?? data?.raw ?? "<h2>Error</h2>No content found on Mounted"
+      editableTitle.value=route.params?.document; console.log(data);console.log("/ getcontent loaded - onBeforeRouteUpdate");
+      isDataLoaded.value = true 
+    }
+    
 })
 onBeforeRouteUpdate(async (to, from) => {
   editableTitle.value=to.params?.document
@@ -150,20 +158,20 @@ const updateContentRef = (html: string, raw?: string) => { content.value=html; c
   }
   25% {
     border-bottom-width: 1px;
-    border-style: solid;
-    border-color: blueviolet;
+    border-bottom-style: solid;
+    border-bottom-color: blueviolet;
     background: linear-gradient(to right, darkgreen 50%, lightblue);
   }
   50% {
     border-bottom-width: 1px;
-    border-style: solid;
-    border-color: blueviolet;
+    border-bottom-style: solid;
+    border-bottom-color: blueviolet;
     background: linear-gradient(to right, darkgreen 25%, lightblue);
   }
   75% {
     border-bottom-width: 1px;
-    border-style: solid;
-    border-color: blueviolet;
+    border-bottom-style: solid;
+    border-bottom-color: blueviolet;
     background: linear-gradient(to right, darkgreen 10%, lightblue);
   }
   from {
