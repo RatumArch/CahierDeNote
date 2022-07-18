d70102dcd59e9d04789dbed20e863f9d3f61ed03
Totaroo <muigni-hazi.mze@cpe.fr>
2022-07-18 18:47:50 +0200

    correction

---
+++ api/findExemple.ts
@@ -0,0 +1,7 @@
+
+export default function verles(req: any, res: any) {
+    const lang = req.query?.lang
+    let textHomePage = 
+        "<h2>Exemples</h2><p>La police par défaut est <strong>Fira Code</strong>, ce qui signifie que vous pouvez écrire des symboles grâce à de belles ligatures :</p><ul><li><p><em>a &gt;= b &amp;&amp; b&gt;=c =&gt; a&gt;=c</em></p></li></ul><p>Si les ligatures ne vous suffisent pas, ce n'est pas grave, car vous pouvez utiliser des écrire des équation en <strong>LaTex</strong>.</p><h2><latex-block rawtext='\\cos_\\theta = \\frac{e^{i\\theta}+e^{-i\\theta}}{2}' latexnodeid='1658160909015'></latex-block></h2><p></p><h2>Images</h2><p>Bien entendu, il est aussi possible d'insérer des images.</p><p>Voici une photo de mangrove.</p><image-input src='https://res.cloudinary.com/dzggewhvt/image/upload/v1658162084/mangrove.png' width='84px'></image-input>"
+    res.status(200).send(textHomePage)
+}
