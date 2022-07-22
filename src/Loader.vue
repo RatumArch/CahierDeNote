f78f58d431917f3ad1f460d7695442aab8e6bc2a
Totaroo <muigni-hazi.mze@cpe.fr>
2022-07-22 17:07:48 +0200

    interval

--- src/Loader.vue
+++ src/Loader.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="loader">
     <div class="pancarte">
-    <h1><span>&#xEE06</span><span>&#xEE07</span><span>&#xEE08</span> <span>&#xEE09</span></h1>
+    <h1><span v-if="tick=0">&#xEE06</span><span v-if="tick=1">&#xEE07</span><span v-if="tick=2">&#xEE08</span> <span v-if="tick=3">&#xEE09</span></h1>
       
         <font-awesome-icon class="icon" icon="fa-solid fa-file-text" />
 
