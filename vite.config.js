2902e6a90c588030039b6d3cb014d3612f8f1f5c
Totaroo <muigni-hazi.mze@cpe.fr>
2022-07-23 21:46:29 +0200

    build options

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,16 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          noteEditor: [ './src/components/NoteEditor.vue' ],
+          laTexNode: [ './src/components/LatexNode.vue' ]
+        }
+      }
+    }
+  },
   envDir: '.',
   plugins: [
     vue(),
