8e0b0bcd55d18d8d0c4abbe1ef6ee9148f294371
Totaroo <muigni-hazi.mze@cpe.fr>
2022-07-23 20:54:45 +0200

    Vite config - Chunks Optimisation. Dynamic import NoteEditor

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createWebHistory, createRouter } from "vue-router";
 
-const Home = () => import('@/Home.vue')
-const Document = () => import('@/Document')
-const Folder = () => import('@/Folder.vue')
-const Error = () => import("@/Error.vue")
+const Home = import('@/Home.vue')
+const Document = import('@/Document')
+const Folder = import('@/Folder.vue')
+const Error = import("@/Error.vue")
 const Loader = () => import('@/Loader.vue')
-const Feedback = () => import('@/Feedback.vue')
+const Feedback = import('@/Feedback.vue')
 
 import axios from "axios";
 
