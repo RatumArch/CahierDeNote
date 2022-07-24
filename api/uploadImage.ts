830a91de61283cbd468c0c32612cd77b41d94d79
Totaroo <muigni-hazi.mze@cpe.fr>
2022-07-24 10:47:38 +0200

    api upload image

--- api/uploadImage.ts
+++ api/uploadImage.ts
@@ -2,6 +2,7 @@
 import {clientPromise} from "../utils";
 import axios from 'axios'
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import { Blob, Buffer } from 'node:buffer';
 const db = process.env.MONGODB_DB
 const collec = process.env.MONGODB_DB_COLLECTION
 
@@ -13,7 +14,8 @@ export default async function insertMongo(req: VercelRequest, res: VercelRespons
  
   const cloudName = 'dzggewhvt'
   console.log(body?.length);
-  const img = Buffer.from(body) ; console.log(img?.length);console.log(img?.byteLength)
+  //const img = Buffer.from(body) ; console.log(img?.length);console.log(img?.byteLength)
+  const img = new Blob([body], {type: 'image'})
   console.log(img);
   
   const upload = await axios.postForm(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, {
