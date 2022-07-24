5fffe7f2efda28c3c9d4dd54bb855194c1083f0d
Totaroo <muigni-hazi.mze@cpe.fr>
2022-07-24 17:06:50 +0200

    api upload image

--- api/uploadImage.ts
+++ api/uploadImage.ts
@@ -2,8 +2,8 @@
 import {clientPromise} from "../utils";
 import axios from 'axios'
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import { Blob, Buffer } from 'node:buffer';
-import {FormData, File} from "formdata-node"
+import { Buffer } from 'node:buffer';
+import { Blob, FormData, File} from "formdata-node"
 const db = process.env.MONGODB_DB
 const collec = process.env.MONGODB_DB_COLLECTION
 
@@ -13,6 +13,7 @@ export default async function insertMongo(req: VercelRequest, res: VercelRespons
   let body = req.body
   const form = new FormData()
   const file = new File([body], "nia.png")
+  const blob = new Blob([body], { type: 'image'})
 
   console.log(Buffer.isBuffer(body))
   const img = Buffer.from(body)
@@ -24,15 +25,16 @@ export default async function insertMongo(req: VercelRequest, res: VercelRespons
 
   form.set("upload_preset", 'ze5mrykg')
   form.set("file", body)
+  
 
-  axios.post(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, form, { headers: {'Content-Type': 'multipart/form-data' }} )
+  axios.post(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, { file: body, upload_preset: 'ze5mrykg'}, { headers: {'Content-Type': 'multipart/form-data' }} )
                     .then((uplo) => {
                       console.log('Première vag')
                       res.status(uplo?.status ?? 505). send({uploadRes: uplo?.data})
                     })
     .catch(err => {  
       console.log('Première catch')
-      axios.post(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, form, { headers: {'Content-Type': 'multipart/form-data' }} )
+      axios.post(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, { file: file, upload_preset: 'ze5mrykg'}, { headers: {'Content-Type': 'multipart/form-data' }} )
                     .then((uplo) => {
                       console.log('Deuxième vag')
                       res.status(uplo?.status ?? 505). send({uploadRes: uplo?.data})
@@ -40,14 +42,14 @@ export default async function insertMongo(req: VercelRequest, res: VercelRespons
       .catch(() => {
         console.log('Deuxième catch')
         form.set("file", file)
-        axios.post(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, form, { headers: {'Content-Type': 'multipart/form-data' }} )
+        axios.post(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, { file: blob, upload_preset: 'ze5mrykg'}, { headers: {'Content-Type': 'multipart/form-data' }} )
                     .then((uplo) => {
                       console.log('Troisième vag')
                       res.status(uplo?.status ?? 505). send({uploadRes: uplo?.data})
                     })
         .catch(() => {
           console.log('Troisième catch')
-          form.set("file", file)
+          form.set("file", blob)
           axios.postForm(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, form )
                     .then((uplo) => {
                       console.log('4ième vag')
